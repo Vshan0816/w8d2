03eef783b7ebd722796277ce1142d0fe298a7fc0
Vincent <vshan0816@gmail.com>
2023-08-15 16:21:50 -0400

    Finish MyEach

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
@@ -1,1 +1,17 @@
 // Your code here
+Array.prototype.myEach = function(callback) {
+    const that = this
+    for (let i = 0; i < that.length; i++) {
+        callback(that[i])
+    }
+};
+
+
+[1,2,3].myEach(function(el) {
+    console.log(el + 1)
+});
+
+Array.myEach(callback)
+
+// ruby - make function take in an array
+// def myFunction(array)
