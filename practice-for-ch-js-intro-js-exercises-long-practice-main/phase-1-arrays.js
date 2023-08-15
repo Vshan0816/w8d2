6978b17c64199e9dc8ee14e1422571b58154bd5c
Vincent <vshan0816@gmail.com>
2023-08-15 15:02:34 -0400

    Finish two sum

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -11,3 +11,18 @@ Array.prototype.uniq = function() {
 };
 
 // console.log([1,2,2,3,3,3].uniq()) // => [1,2,3]
+Array.prototype.twoSum = function() {
+    let pairs = []
+    const that = this
+
+    that.forEach(function(el1, i1) {
+        that.forEach(function(el2, i2) {
+            if (i1<i2 && el1+el2===0) {
+                pairs.push([i1, i2])
+            }
+        })
+    })
+    return pairs
+};
+
+console.log([-1, 0, 2, -2, 1].twoSum()) // => [[0,4] [2,3]]
