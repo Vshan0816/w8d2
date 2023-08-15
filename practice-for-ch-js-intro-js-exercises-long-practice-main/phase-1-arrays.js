38819c8e73097b9ddcb7ffee7469c2194e8d38d8
Dan Lee <danielhlee4@gmail.com>
2023-08-15 14:38:53 -0400

    add Array.prototype.uniq

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -1,1 +1,13 @@
 // Your code here
+Array.prototype.uniq = function() {
+    let new_arr = []
+
+    this.forEach(function(el) {
+        if (!new_arr.includes(el)) {
+            new_arr.push(el)
+        }
+    })
+    return new_arr
+};
+
+// console.log([1,2,2,3,3,3].uniq()) // => [1,2,3]
