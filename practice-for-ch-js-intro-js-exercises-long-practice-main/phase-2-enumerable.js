751e1bec6c00a9b0e1d3ff32d910ea960b3546d6
Dan Lee <danielhlee4@gmail.com>
2023-08-15 16:46:54 -0400

    start myMap

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
@@ -6,12 +6,27 @@ Array.prototype.myEach = function(callback) {
     }
 };
 
+// [1,2,3].myEach(function(el) {
+//     console.log(el + 1)
+// });
 
-[1,2,3].myEach(function(el) {
-    console.log(el + 1)
-});
-
-Array.myEach(callback)
+// Array.myEach(callback)
 
 // ruby - make function take in an array
 // def myFunction(array)
+
+Array.prototype.myMap = function(callback) {
+    let that = this
+    let new_arr = []
+    that.myEach(function(el) { 
+        new_arr.push(callback(el))
+    })
+    return new_arr
+};
+
+// callback here is the function passed into myMap which 
+// will determine how the el is mapped into the new arr
+
+console.log([1,2,3].myMap(function(el) {
+    el * 2
+}));
